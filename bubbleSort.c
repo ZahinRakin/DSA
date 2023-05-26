cc795dfd59cfdd5f82738d1db430e388501117c2
zahin abdullah rakin <zahinabdullahrakin@gmail.com>
2023-05-26 23:39:09 +0600

    new addition of bubble sort

--- bubbleSort.c
+++ bubbleSort.c
@@ -4,16 +4,16 @@ void bubble(int* , int);
 void swap(int* , int*);
 
 void main(){
-    int arr[] = {2,4,3};
-    bubble(arr , 3);
-    for(int i = 0 ; i < 3 ; i++)
+    int arr[] = {2,4,3,0,1,2};
+    bubble(arr , 6);
+    for(int i = 0 ; i < 6 ; i++)
         printf("%d ",arr[i]);
 }
 void bubble(int* arr , int size){
     for(int i = 1 ; i < size ; i++){
-        for(int j = size - 1 ; j > i ; j--){
+        for(int j = size - 1 ; j >= i ; j--){
             if(arr[j] < arr[j - 1])
-                swap(&arr[j] , &arr[i]);
+                swap(&arr[j] , &arr[j-1]);
         }
     }
 }
